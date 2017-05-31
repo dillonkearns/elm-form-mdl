0a862109f5554b28d65004599d5b2ddca25bc778
Dillon Kearns <dillon@dillonkearns.com>
2017-05-30 21:01:16 -0400

    Render text input field

--- src/Main.elm
+++ src/Main.elm
@@ -1,28 +1,69 @@
 module Main exposing (main)
 
+import Form exposing (Form)
+import Form.Input as Input
+import Form.Validate exposing (Validation)
 import Html exposing (..)
+import Html.Attributes
+
+
+-- import Html.Attributes
 
 
 type Msg
     = NoOp
+    | FormMsg Form.Msg
+
+
+type alias UserForm =
+    { name : String
+    }
 
 
 type alias Model =
-    { name : String
+    { form : Form () UserForm
     }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ text "hello" ]
+        [ h1 [] [ text "hello" ]
+        , Html.map FormMsg (formView model.form)
+        ]
+
+
+errorFor field =
+    case field.liveError of
+        Just error ->
+            -- replace toString with your own translations
+            div [ Html.Attributes.style [ ( "color", "red" ) ] ] [ text (toString error) ]
+
+        Nothing ->
+            text ""
+
+
+formView : Form () UserForm -> Html Form.Msg
+formView form =
+    let
+        nameField =
+            Form.getFieldAsString "name" form
+    in
+    Html.form []
+        [ Input.textInput nameField []
+        , errorFor nameField
+        ]
 
 
 init : ( Model, Cmd Msg )
 init =
-    { name = ""
-    }
+    { form = Form.initial [] userFormValidation }
         ! []
+
+
+userFormValidation : Validation () UserForm
+userFormValidation =
+    Form.Validate.map UserForm (Form.Validate.field "name" Form.Validate.email)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -30,6 +71,9 @@ update msg model =
     case msg of
         NoOp ->
             model ! []
+
+        FormMsg formMsg ->
+            ( { model | form = Form.update userFormValidation formMsg model.form }, Cmd.none )
 
 
 main : Program Never Model Msg
